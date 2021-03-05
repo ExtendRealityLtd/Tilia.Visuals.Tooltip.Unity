3b5267ee7555c0395603ca286ad2d03474285c8b
extendreality <hello@extendreality.io>
2021-03-05 10:37:44 +0000

    feat(TextProcessing): add ability to defer text processing
    The new TextProcessor concept allows the text processing to be deferred
    to a Text Processor type. The Unity UI Text type is provided by default
    as it is the current functionality. But it would now be possible and
    trivial to make a Text Processor for TextMeshPro that could then be
    used on the tooltip without needing to include TextMeshPro with this
    repo.

---
+++ Runtime/SharedResources/Scripts/TextProcessing/UITextProcessor.cs
@@ -0,0 +1,39 @@
+﻿namespace Tilia.Visuals.Tooltip.TextProcessing
+{
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    /// <summary>
+    /// A text processor that uses the Unity UI <see cref="Text"/> element for display.
+    /// </summary>
+    public class UITextProcessor : BaseTextProcessor
+    {
+        /// <summary>
+        /// The <see cref="Text"/> to render onto the tooltip.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml]
+        public Text TooltipText { get; set; }
+
+        /// <inheritdoc />
+        public override void SetColor(Color color)
+        {
+            TooltipText.color = color;
+        }
+
+        /// <inheritdoc />
+        public override void SetSize(float size)
+        {
+            RectTransform rect = TooltipText.GetComponent<RectTransform>();
+            TooltipText.fontSize = Mathf.RoundToInt(size / rect.localScale.x);
+        }
+
+        /// <inheritdoc />
+        public override void SetText(string text)
+        {
+            TooltipText.text = text;
+        }
+    }
+}
